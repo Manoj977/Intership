a4e50eb92a44e038aa4aa5a69464389278b44650
Manoj S <manojsaravanan212@gmail.com>
2022-10-31 10:14:49 +0530

    commit

--- Day_16/006/script.js
+++ Day_16/006/script.js
@@ -3,10 +3,10 @@ function assign() {
   document.getElementById("count").innerText = data;
 }
 function inc() {
-  data = data + 1;
+  data += 1;
   assign();
 }
 function dec() {
-  data = data - 1;
+  data -= 1;
   assign();
 }
