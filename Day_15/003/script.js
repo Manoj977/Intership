044afc423895846f55d637bc3634c472baf29c9a
Manoj S <manojsaravanan212@gmail.com>
2022-10-31 10:03:50 +0530

    commit

--- Day_15/003/script.js
+++ Day_15/003/script.js
@@ -5,62 +5,49 @@ var result,
   odd = 0,
   even = 0,
   zero = 0,
-  total=20;
-for (var i = 0; i < 20; i++) {
-  var a = i + 1;
-  var number = parseInt(prompt("Enter your " + a + " number"));
-  n.push(number);
-}
+  total = 20;
 
-for (var i = 0; i < total; i++) {
-  // if (n.includes(NaN) == false) {
-  if (n[i] < 0) ++negative;
-  // } else {
-  //   n.find((notavl) => notavl == NaN);
-  //   var not = n.indexOf(NaN);
-  //   console.log("number is not available in" + n + "th number");
-  // }
+// for (var i = 0; i < 20; i++) {
+//   var a = i + 1;
+//   var number = parseInt(prompt("Enter your " + a + " number"));
+//   n.push(number);
+// }
+var number = prompt("Enter 20 Numbers");
+var arrayReturn = (values) => Number(values);
+n = Array.from(number, arrayReturn);
+if (n.length == 20) {
+  for (var i = 0; i < total; i++) {
+    if (n[i] < 0) ++negative;
+  }
+  for (var i = 0; i < total; i++) {
+    if (n[i] > 0) ++positive;
+  }
+  for (var i = 0; i < total; i++) {
+    if (n[i] == 0) ++zero;
+  }
+  for (var i = 0; i < total; i++) {
+    if (n[i] % 2 == 0) ++even;
+  }
+  for (var i = 0; i < total; i++) {
+    if (n[i] % 2 != 0) ++odd;
+  }
+  console.log("Your Entered Digits are: " + n);
+  document.getElementById("total").innerHTML = n;
+  console.log("no.of.-ve numbers are: " + negative);
+  document.getElementById("negative").innerHTML = negative;
+  console.log("no.of.+ ve numbers are: " + positive);
+  document.getElementById("positive").innerHTML = positive;
+  console.log("no.of.zeros: " + zero);
+  document.getElementById("zeros").innerHTML = zero;
+  console.log("no.of.odd numbers: " + odd);
+  document.getElementById("odd").innerHTML = odd;
+  console.log("no.of.even numbers: " + even);
+  document.getElementById("even").innerHTML = even;
+} else if (n.length < 20) {
+  document.getElementById("total").innerHTML =
+    "Your Entered Digits are lesser than 20";
 }
-for (var i = 0; i < total; i++) {
-  // if (n.includes(NaN) == false) {
-  if (n[i] > 0) ++positive;
-  // } else {
-  //   n.find((notavl) => notavl == NaN);
-  //   var not = n.indexOf(NaN);
-  //   console.log("number is not available in" + n + "th number");
-  // }
+else if (n.length > 20) {
+  document.getElementById("total").innerHTML =
+    "Your Entered Digits are Greater than 20";
 }
-for (var i = 0; i < total; i++) {
-  // if (n.includes(NaN) == false) {
-  if (n[i] == 0) ++zero;
-  // } else {
-  //   n.find((notavl) => notavl == NaN);
-  //   var not = n.indexOf(NaN);
-  //   console.log("number is not available in" + n + "th number");
-  // }
-}
-for (var i = 0; i < total; i++) {
-  // if (n.includes(NaN) == false) {
-  if (n[i] % 2 == 0) ++even;
-  // } else {
-  //   n.find((notavl) => notavl == NaN);
-  //   var not = n.indexOf(NaN);
-  //   console.log("number is not available in" + n + "th number");
-  // }
-}
-for (var i = 0; i < total; i++) {
-  // if (n.includes(NaN) == false) {
-  if (n[i] % 2 != 0) ++odd;
-  // } else {
-  //   n.find((notavl) => notavl == NaN);
-  //   var not = n.indexOf(NaN);
-  //   console.log("number is not available in" + n + "th number");
-  // }
-}
-
-console.log("Your Entered Digits are: " + n);
-console.log("no.of.-ve numbers are: " + negative);
-console.log("no.of.+ ve numbers are: " + positive);
-console.log("no.of.zeros: " + zero);
-console.log("no.of.odd numbers: " + odd);
-console.log("no.of.even numbers: " + even);
