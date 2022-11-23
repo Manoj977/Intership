53932fb68fc8833942a0e778234c631f4b310536
Manoj S <manojsaravanan212@gmail.com>
2022-11-23 15:22:33 +0530

    commit

--- Advanced Concept REACT/ReactIntro/react-session/src/View.jsx
+++ Advanced Concept REACT/ReactIntro/react-session/src/View.jsx
@@ -4,61 +4,35 @@ const View = () => {
   const allProducts = JSON.parse(localStorage.getItem("products"));
   return (
     <div className="view">
-      <form>
-        <table>
-          <tbody>
-            {allProducts.map((item, index) => (
+      {allProducts.map((item, index) => (
+        <form key={item}>
+          <table>
+            <tbody>
               <tr>
                 <td>
-                  <label htmlFor="product-id">Product ID</label>
-                  <div id="product-id">{item.ID}</div>
-                </td>
-                <td>
-                  <label htmlFor="product-Name">Product Name</label>
-                  <div id="product-id">{item.Name}</div>
-                </td>
-                <td>
-                  <div id="product-id">
-                    <div id="product-image">
-                      <img src={item.Image} alt="" />
-                    </div>
+                  <div className="flex">
+                    <label htmlFor="product-id">Product ID</label>
+                    <div id="product-id">{item.ID}</div>
+                  </div>
+                  <div className="flex">
+                    <label htmlFor="product-Name">Product Name</label>
+                    <div id="product-id">{item.Name}</div>
+                  </div>
+                  <div className="flex">
+                    <label htmlFor="product-image">Product Image</label>
+
+                    <img id="product-image" src={item.Image} alt="" />
+                  </div>
+                  <div className="flex">
+                    <label htmlFor="product-Price">Product Price</label>
+                    <div id="product-price">₹{item.Price}</div>
                   </div>
                 </td>
-                <td>
-                  <label htmlFor="product-Price">Product Price</label>
-                  <div id="product-price">₹{item.Price}</div>
-                </td>
               </tr>
-            ))}
-            {/* <tr>
-              <td>
-                <label htmlFor="product-ID">Product ID</label>
-                <div id="product-ID">{localStorage.getItem("ID")}</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="product-Name">Product Name</label>
-                <div id="product-name">{localStorage.getItem("Name")}</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="product-Image">Product Image</label>
-                <div id="product-image">
-                  <img src={localStorage.getItem("Image")} alt="" />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="product-Price">Product Price</label>
-                <div id="product-price">{localStorage.getItem("Price")}</div>
-              </td>
-            </tr> */}
-          </tbody>
-        </table>
-      </form>
+            </tbody>
+          </table>
+        </form>
+      ))}
     </div>
   );
 };
