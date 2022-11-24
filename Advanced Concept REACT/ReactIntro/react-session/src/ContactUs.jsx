2f8d8c33498ba389ab5dc579094dc2aa217ffa29
Manoj S <manojsaravanan212@gmail.com>
2022-11-24 08:26:46 +0530

    COMMIT

--- Advanced Concept REACT/ReactIntro/react-session/src/ContactUs.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ContactUs = () => {
-  return (
-    <div className="content container">ContactUs</div>
-  )
-}
-
-export default ContactUs
--- Advanced Concept REACT/ReactIntro/react-session/src/Main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Main = () => {
-  return <div className="content">Main</div>;
-};
-
-export default Main;
