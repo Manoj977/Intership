e514cce41837e88a6e1f29ddd185d9573e9a2eb1
Manoj S <manojsaravanan212@gmail.com>
2022-11-24 09:56:24 +0530

    commit

---
+++ Advanced Concept REACT/ReactIntro/react-session/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Page (Tailwind-CSS)/Home";
+
+const App = () => {
+  return (
+    <>
+      next
+    </>
+  );
+};
+
+export default App;
---
+++ Advanced Concept REACT/ReactIntro/react-session/src/Page (Tailwind-CSS)/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="lg:flex text-center  lg:justify-center lg:items-center w-full border-2 h-screen transition-all ease-in-out  inline-grid place-items-center">
+        <div className="text-yellow font-semibold text-center m-2 sm: bg-purple md:bg-pink lg:bg-green p-3 rounded-sm flex items-center justify-center">
+          Manoj
+        </div>
+        <div className="text-yellow font-semibold text-center m-2 sm: bg-purple md:bg-pink lg:bg-green p-3 rounded-sm flex items-center justify-center">
+          Manoj
+        </div>
+        <div className="text-yellow font-semibold text-center m-2 sm: bg-purple md:bg-pink lg:bg-green p-3 rounded-sm flex items-center justify-center">
+          Manoj
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
